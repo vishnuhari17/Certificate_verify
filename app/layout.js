9a85e01c9b324b60d7f7b7c8bab69e88b79dc5b4
vishnuhari17 <vishnuhari.anup@gmail.com>
2024-04-14 15:20:22 +0530

    Added Metadata

--- app/layout.js
+++ app/layout.js
@@ -7,28 +7,50 @@ import Particle from "../app/Particle";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Certificate Verification",
-  description: "Verify Certificates Instantly and Also Find Certificates by Participant Name a",
   openGraph: {
+    title: 'Certificate Verification',
+    description: 'Verify Certificates Instantly and Also Find Certificates by Participant Name',
+    url: 'https://certificate-verify-kappa.vercel.app/',
+    siteName: 'Certificate Verify',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/vishnuhari17/Certificate_verify/main/image.png?token=GHSAT0AAAAAACPXPPR4ALH3G273BJKCDLOMZQ3UU3Q', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      }
+    ],
+    locale: 'en_US',
     type: 'website',
-    url: 'https://metatags.io/',
-    title: 'Certificate Verification',
-    description: 'Verify Certificates Instantly and Also Find Certificates by Participant Name a',
-    image: 'https://metatags.io/images/meta-tags.png',
   },
-  twitter: {
-    card: 'summary_large_image',
-    url: 'https://metatags.io/',
-    title: 'Certificate Verification',
-    description: 'Verify Certificates Instantly and Also Find Certificates by Participant Name a',
-    image: 'https://metatags.io/images/meta-tags.png',
-  },
-};
+}
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content="Web site created using create-react-app" />
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        <meta name="description" content="Self Developed personal website build with React.js" />
+
+
+        <meta itemprop="name" content="S0umyajit | Portfolio" />
+        <meta itemprop="description" content="Self Developed personal website build with React.js" />
+        <meta itemprop="image"
+          content="https://raw.githubusercontent.com/vishnuhari17/Certificate_verify/main/image.png?token=GHSAT0AAAAAACPXPPR4ALH3G273BJKCDLOMZQ3UU3Q" />
+
+        <meta property="og:url" content="https://soumyajit.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="S0umyajit | Portfolio" />
+        <meta property="og:description" content="Self Developed personal website build with React.js" />
+        <meta property="og:image" content="https://raw.githubusercontent.com/vishnuhari17/Certificate_verify/main/image.png?token=GHSAT0AAAAAACPXPPR4ALH3G273BJKCDLOMZQ3UU3Q" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="S0umyajit | Portfolio" />
+        <meta name="twitter:description" content="Self Developed personal website build with React.js" />
+        <meta name="twitter:image"
+          content="https://raw.githubusercontent.com/vishnuhari17/Certificate_verify/main/image.png?token=GHSAT0AAAAAACPXPPR4ALH3G273BJKCDLOMZQ3UU3Q" />
+      </head>
       <body className={inter.className}>
         <Particle id='particles' />
         {children}
